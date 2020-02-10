e42649f48a82adcc9d3e5d34b44eb77b8de7a11b
irina08 <irina_ger@yahoo.com>
2020-02-10 15:27:28 -0800

    added 1 end-to-end test for Create New FlashGroup with 1 required field

---
+++ test/specs/flashcards/cards_data.js
@@ -0,0 +1,9 @@
+const gname = 'QA' + Math.floor(Math.random() * 100);
+
+const cardsPage = {
+    h1: 'FlashCards',
+    h5: 'Create Flash Group',
+    groupName: gname,
+    groupDescription: 'The best QA group in the world',
+};
+ module.exports = { cardsPage };
